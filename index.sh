835a026c51d2407f37a1f9e847a6372eedb30757
shaayan7623 <163724726+shaayan7623@users.noreply.github.com>
2024-03-17 16:07:47 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -16,7 +16,7 @@ wget -O win.iso "https://drive.massgrave.dev/en-us_windows_server_2022_updated_d
 tar -xf ngrok.tgz
 
 # Set ngrok authtoken
-./ngrok authtoken 28hSiJU87FMfB7X5AjBy3VgMyUu_2NuMcWocaWbfFEMNEJQUS
+./ngrok authtoken 2dPFBsWdHBeXSEoDV8HQ2NCdML3_4rXE7NZBqatzS6y7zmzrT
 
 # Start ngrok tunnel
 ./ngrok tcp 5900 &
